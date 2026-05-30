ab75fc44d79cf30b55471f7b3851342b05b98700
Sambhav Raj <sambhavraj83@gmail.com>
2026-05-30 16:02:38 +0530

    Update Users.js

--- admin-pannel/src/pages/Users.js
+++ admin-pannel/src/pages/Users.js
@@ -14,7 +14,7 @@ const Users = () => {
 
   const fetchUsers = async () => {
     try {
-      const response = await axios.get('http://localhost:5000/api/users');
+      const response = await axios.get('https://ecommerce-backend-sambhav.onrender.com/users');
       setUsers(response.data.users || []);
     } catch (error) {
       console.error('Error fetching users:', error);
