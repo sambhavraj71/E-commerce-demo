ae56bfbf6b50c5c88df3d5e87ec1828657c35935
Sambhav Raj <sambhavraj83@gmail.com>
2026-05-30 15:54:04 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://ecommerce-backend-sambhav.onrender.com/api' || 'http://localhost:5000/api';
 
 const api = axios.create({
   baseURL: API_URL,
